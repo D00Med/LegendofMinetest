c53462d508c9e2988d4f9c5aed582452f8636d02
Christopher Wells <cwellsny@nycap.rr.com>
2017-01-10 18:12:06 -0500

    Fix steel boomerang name in loot mod
    Fix the naming of the Steel Boomerang in the dungeon loot mod.

--- mods/dungeon_loot/init.lua
+++ mods/dungeon_loot/init.lua
@@ -28,7 +28,7 @@ local chest_stuff = {
 	{name="maptools:nyan_coin", max = 5},
 	{name="twilight:crystal", max = 1},
 	{name="hyruletools:triforce_shard", max = 1},
-	{name="hyruletools:steel_boomerang", max = 1},
+	{name="hyruletools:boomerang_steel", max = 1},
 	{name="default:ice", max = 3}
 }
 
