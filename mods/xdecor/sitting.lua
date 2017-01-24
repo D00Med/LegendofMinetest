d4b285c9c675c7b24e57d4fe7d1da335e6f60f9f
D00Med <heiselong@gmx.com>
2017-01-25 07:15:14 +1000

    chair fix attempt

--- mods/xdecor/sitting.lua
+++ mods/xdecor/sitting.lua
@@ -4,7 +4,6 @@ local function sit(pos, node, clicker)
 		pos.y = pos.y - 0.5
 		clicker:setpos(pos)
 		clicker:set_eye_offset({x=0, y=0, z=0}, {x=0, y=0, z=0})
-		clicker:set_physics_override(1, 1, 1)
 		default.player_attached[player] = false
 		default.player_set_animation(clicker, "stand", 30)
 	elseif default.player_attached[player] ~= true and node.param2 <= 3 and
@@ -13,7 +12,6 @@ local function sit(pos, node, clicker)
 			clicker:get_player_velocity().z == 0 then
 
 		clicker:set_eye_offset({x=0, y=-7, z=2}, {x=0, y=0, z=0})
-		clicker:set_physics_override(0, 0, 0)
 		clicker:setpos(pos)
 		default.player_attached[player] = true
 		default.player_set_animation(clicker, "sit", 30)
